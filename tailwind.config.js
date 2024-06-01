4beebd34191b3cf5b408ef509b4cdf59495641ea
Vigneshreddy <vigneshreddy625@gmail.com>
2024-06-01 18:20:34 +0530

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors : {
+      'bc' : '#006780',
+      'bsc' : '#a4ede9',
+      'white' : '#ffffff',
+      'ubc' : '#95dddc',
+      'brc' : '#076d85',
+      'blc' : '#e3f0f0',
+      'black' : '#0a0a0a',
+      'sn' : '#139faa'
+    }
+  },
+  plugins: [],
+}
+
